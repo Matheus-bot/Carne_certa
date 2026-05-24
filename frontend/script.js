172cdec4466fc6fd38c4f0ca9b3ae25496354f5d
Matheus <matheushenrry00@gmail.com>
2026-05-24 17:37:34 -0300

    feat: adiciona painel interativo de preferências

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,80 @@
-console.log('CarneCerta iniciado com sucesso 🚀');
+console.log("CarneCerta iniciado 🚀");
+
+// ABRIR PAINEL
+
+const openPanel =
+document.getElementById("openPanel");
+
+// PAINEL
+
+const preferencesPanel =
+document.getElementById("preferencesPanel");
+
+// FECHAR
+
+const closePanel =
+document.getElementById("closePanel");
+
+// OVERLAY
+
+const overlay =
+document.getElementById("overlay");
+
+// ABRIR PAINEL
+
+openPanel.onclick = () => {
+
+  preferencesPanel.classList.add("active");
+
+  overlay.classList.add("active");
+
+};
+
+// FECHAR NO X
+
+closePanel.onclick = () => {
+
+  preferencesPanel.classList.remove("active");
+
+  overlay.classList.remove("active");
+
+};
+
+// FECHAR CLICANDO NO FUNDO
+
+overlay.onclick = () => {
+
+  preferencesPanel.classList.remove("active");
+
+  overlay.classList.remove("active");
+
+};
+// BOTÕES DE OPÇÃO
+
+const optionButtons =
+document.querySelectorAll(".option-btn");
+
+// CLICOU
+
+optionButtons.forEach((button) => {
+
+  button.onclick = () => {
+
+    // REMOVE DOS OUTROS
+
+    const siblings =
+    button.parentElement.querySelectorAll(".option-btn");
+
+    siblings.forEach((btn) => {
+
+      btn.classList.remove("active-option");
+
+    });
+
+    // ADICIONA NO CLICADO
+
+    button.classList.add("active-option");
+
+  };
+
+});
